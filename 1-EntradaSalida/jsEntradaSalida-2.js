7f3ecdfd9e34c964d6615af4aa315113e017062e
Sebastián Hidalgo <sebahidalgo@live.com.ar>
2017-02-02 20:23:01 -0300

    Primer Commit
    Hicimos el ejercicio 1 y 2 en clase.

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,16 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
-}
+	//w3school fuente deinformacion javascript
+
+	//F12 -> Console -> muestra los errores de sintaxis del codigo.
+
+	//var nombre;
+	//alert(nombre);
+	var DataToShow; 
+
+	DataToShow = prompt("Ingresar el nombre: ", "N/N");
+
+	alert(DataToShow);
+}	
 
