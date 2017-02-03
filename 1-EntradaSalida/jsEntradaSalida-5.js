29cd65ae77f69e565de45b44af3f286aa97e6de0
Sebastián Hidalgo <sebahidalgo@live.com.ar>
2017-02-03 13:40:32 -0300

    Update jsEntradaSalida-5.js

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,11 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostar()
 {	
+	var edad = getElementById('laEdad').value;
+	var nombre = getElementById('elNombre').value;
 	
+	var concatenated = "Usted se llama " + nombre + " y tiene " + edad + " años";
+	
+	alert(concatenated);
 }
 
